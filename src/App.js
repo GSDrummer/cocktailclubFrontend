6a67602e683fa44f99680bf8190a2ba6aad7d8b1
Josh Howcroft <Josh@Joshs-iMac.local>
2021-06-16 17:56:32 +0100

    # minor tweak to search

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import { signUp, userLogin } from "./utils/index";
 import Search from "./components/Search";
 
 const App = () => {
-  const [user, setUser] = useState();
   const [data, setData] = useState([]);
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [search, setSearch] = useState("");
 
   return (
     <div className="App">
