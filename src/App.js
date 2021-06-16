77c549182a63d844bda564c4017de9049a195082
Josh Howcroft <Josh@Joshs-iMac.local>
2021-06-16 17:56:05 +0100

    search function fixed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import React, { useState } from "react";
 import { signUp, userLogin } from "./utils/index";
 
-import Search from "./Components/Drinks";
+import Search from "./components/Search";
 
 const App = () => {
   const [user, setUser] = useState();
@@ -11,36 +11,11 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [search, setSearch] = useState("");
 
-  const handleFetch = async () => {
-    try {
-      const response = await fetch(
-        `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`
-      );
-      console.log(response);
-      if (response.status !== 200) {
-        throw new Error("Failed to fetch");
-      }
-
-      const data = await response.json();
-      // console.log(data);
-      setData(data);
-      setLoading(true);
-    } catch (err) {
-      console.log(err.message);
-      setError(err.message);
-    }
-  };
-
-  if (error) return <h1>{error}</h1>;
-  if (!loading)
-    return (
-      <div className="App">
-        <div>
-          <button onClick={handleFetch}>Search for a cocktail</button>
-        </div>
-        <Search drinks={data} />
-      </div>
-    );
+  return (
+    <div className="App">
+      <Search drinks={data} />
+    </div>
+  );
 };
 
 export default App;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,41 @@
 import React, { useState } from "react";
 
-const Search = async ({ drinks }) => {
+const Search = () => {
   const [search, setSearch] = useState("");
+  const [data, setData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
-    // hi hi
-    const matches = drinks.filter((item) => item === search);
-    console.log(matches);
+  const handleFetch = async () => {
+    try {
+      const response = await fetch(
+        `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`
+      );
+      console.log(response);
+      if (response.status !== 200) {
+        throw new Error("Failed to fetch");
+      }
+
+      const data = await response.json();
+      // console.log(data);
+      setData(data);
+      setLoading(false);
+    } catch (err) {
+      console.log(err.message);
+      setError(err.message);
+    }
   };
 
+  const handleSubmit = (e) => {
+    handleFetch();
+    e.preventDefault();
+
+    // const matches = data?.drinks[0]?.strDrink?.filter(
+    //   (item) => item === search
+    // );
+    // console.log(matches);
+  };
+  if (error) return <h1>{error}</h1>;
   return (
     <div>
       <form onSubmit={handleSubmit}>
@@ -25,26 +50,31 @@ const Search = async ({ drinks }) => {
         </label>
         <button type="submit">Search</button>
       </form>
-      <div>
-        <div className="drink-name"> {data.drinks[0].strDrink}</div>
+      {loading ? (
+        <div>Loading</div>
+      ) : (
         <div>
-          <img
-            className="drink-thumb"
-            alt="drink"
-            src={data.drinks[0].strDrinkThumb}
-          ></img>
+          <div className="drink-name"> {data.drinks[0].strDrink}</div>
+          <div>
+            <img
+              className="drink-thumb"
+              alt="drink"
+              src={data.drinks[0].strDrinkThumb}
+            ></img>
+          </div>
+          <div className="ingredients">
+            Ingredients: {data.drinks[0].strIngredient1},
+            {data.drinks[0].strIngredient2},{data.drinks[0].strIngredient3},
+            {data.drinks[0].strIngredient4}
+          </div>
+          <div className="glass-type">Glassware: {data.drinks[0].strGlass}</div>
+          <div className="drink-method">
+            Instructions: {data.drinks[0].strInstructions}
+          </div>
         </div>
-        <div className="ingredients">
-          Ingredients: {data.drinks[0].strIngredient1},
-          {data.drinks[0].strIngredient2},{data.drinks[0].strIngredient3},
-          {data.drinks[0].strIngredient4}
-        </div>
-        <div className="glass-type">Glassware: {data.drinks[0].strGlass}</div>
-        <div className="drink-method">
-          Instructions: {data.drinks[0].strInstructions}
-        </div>
-      </div>
+      )}
     </div>
   );
 };
+
 export default Search;
