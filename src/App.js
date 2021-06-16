e5ef5f2126364cc6d7f9944894eccef856f7beab
Josh Howcroft <Josh@Joshs-iMac.local>
2021-06-16 12:38:41 +0100

    initial react logic

--- src/App.js
+++ src/App.js
@@ -4,7 +4,40 @@ import { signUp, userLogin } from "./utils/index";
 
 const App = () => {
   const [user, setUser] = useState();
-  return <div className="App"></div>;
+  const [user, setUser] = useState();
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
+
+  const handleFetch = async () => {
+    try {
+      const response = await fetch(
+        `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`
+      );
+      console.log(response);
+      if (response.status !== 200) {
+        throw new Error("Failed to fetch");
+      }
+
+      const data = await response.json();
+      // console.log(data);
+      setData(data);
+      setLoading(true);
+      setSearch();
+    } catch (err) {
+      console.log(err.message);
+      setError(err.message);
+    }
+  };
+
+  if (error) return <h1>{error}</h1>;
+  if (!loading)
+    return (
+      <div className="App">
+        <Search drinks={data} />
+      </div>
+    );
 };
 
 export default App;
---
+++ src/components/Search.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+const Search = async ({ drinks }) => {
+  const [search, setSearch] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+    // hi hi
+    const matches = drinks.filter((item) => item === search);
+    console.log(matches);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label className="search-label">
+          Search for a Cocktail:
+          <input
+            type="text"
+            name="search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          ></input>
+        </label>
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
+  //   return (
+  //     <div>
+  //       <div className="drink-name"> {data.drinks[0].strDrink}</div>
+  //       <div>
+  //         <img
+  //           className="drink-thumb"
+  //           alt="drink"
+  //           src={data.drinks[0].strDrinkThumb}
+  //         ></img>
+  //       </div>
+  //       <div className="ingredients">
+  //         Ingredients: {data.drinks[0].strIngredient1},
+  //         {data.drinks[0].strIngredient2},{data.drinks[0].strIngredient3},
+  //         {data.drinks[0].strIngredient4}
+  //       </div>
+  //       <div className="glass-type">Glassware: {data.drinks[0].strGlass}</div>
+  //       <div className="drink-method">
+  //         Instructions: {data.drinks[0].strInstructions}
+  //       </div>
+  //     </div>
+  //   );
+};
+export default Search;
